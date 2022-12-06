af1ecb8e2dad8ee531fcf16d71c334a7f6383bb4
Paddy Mullen <paddy@paddymullen.com>
2022-12-06 17:47:43 -0500

    chore: removed bak directory (#174)

--- bak/platform-sdk/src/DataAbstraction/constants.ts
+++
@@ -1,3 +0,0 @@
-export const TEST_DB = 'mysql://ephemeral:ephemeral@localhost:3306'
-export const RECORD_UPDATE_CHUNK_SIZE = 1000
-export const RECORD_BULK_UPDATE_CHUNK_SIZE = 100000
--- bak/platform-sdk/test/jest-init.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-process.env.FLATFILE_ALLOWED_EMAIL_DOMAINS = 'flatfile.io'
-process.env.LAMBDA_ARN = 'local'
