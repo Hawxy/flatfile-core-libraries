a30b3e6b2b36fb5f34281d07f44f74604e53586c
Maire Sangster <mairechew@users.noreply.github.com>
2023-02-28 16:29:49 -0700

    Export theme generator (#334)
    * export theme generator
    
    * export theme generator
    
    ---------
    
    Co-authored-by: Maire Sangster <mairesangster@Maires-MacBook-Pro-2.local>

---
+++ .changeset/wicked-socks-sleep.md
@@ -0,0 +1,5 @@
+---
+'@flatfile/react': patch
+---
+
+Export useThemeGenerator hook
--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,10 +1,11 @@
 import Space from './src/components/Space'
 
+import { useThemeGenerator } from './src/hooks/useThemeGenerator'
 import { useSpace } from './src/hooks/useSpace'
 import type { ISpaceConfig } from './src/types/ISpaceConfig'
 import type { ISidebarConfig } from './src/types/ISidebarConfig'
 import type { IThemeConfig } from './src/types/IThemeConfig'
 
-export { useSpace, Space }
+export { useThemeGenerator, useSpace, Space }
 
 export type { ISpaceConfig, ISidebarConfig, IThemeConfig }
