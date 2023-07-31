4dd82f176654013cee5fa4bbec8efaf2abce2c34
Merit <11151764+meritmalling@users.noreply.github.com>
2023-07-31 08:30:36 -0700

    Update spinner css (#692)
    * Update spinner css
    * Add identifiers
    
    ---------
    
    Signed-off-by: Merit <merit@flatfile.io>

--- packages/react/src/components/Spinner.tsx
+++ packages/react/src/components/Spinner.tsx
@@ -1,63 +1,40 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import '../index.css'
 
 type SpinnerProps = {
   size?: number
 }
 
-const rotateAnimation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
+const StyledSpinner = styled.div<SpinnerProps>`
+  border: 4px solid rgba(255, 255, 255, 0.7);
+  border-top: 4px solid var(--ff-primary-color);
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 999;
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
   }
 `
 
-const dashAnimation = keyframes`
-  0% {
-    stroke-dasharray: 1, 150;
-    stroke-dashoffset: 0;
-  }
-  50% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -35;
-  }
-  100% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -124;
-  }
-`
-
-const StyledSpinner = styled.svg<SpinnerProps>`
-  animation: ${rotateAnimation} 1.5s linear infinite;
-  flex-shrink: 0;
-  font-size: ${({ size }) => size}px;
-  height: 1em;
-  width: 1em;
-
-  circle {
-    stroke: var(--color-electric-800);
-    stroke-linecap: round;
-    animation: ${dashAnimation} 1.8s ease-in-out infinite;
-  }
-`
-
-const Spinner = ({ size = 36 }: SpinnerProps) => {
-  return (
-    <StyledSpinner
-      className="spinner"
-      data-testid="spinner-icon"
-      fill="none"
-      role="img"
-      size={size}
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="12" cy="12" r="8" fill="none" strokeWidth="2" />
-    </StyledSpinner>
-  )
+const Spinner = () => {
+  return <StyledSpinner 
+    className="spinner"
+    data-testid="spinner-icon"
+  >
+  </StyledSpinner>
 }
 
 export default Spinner
