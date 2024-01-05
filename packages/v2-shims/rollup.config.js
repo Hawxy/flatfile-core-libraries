49c47a263ace9d0bd08d86ff2080cb9d27f4b5e5
Alex Rock <hi@lxrck.com>
2024-01-05 10:44:33 -0500

    feat: Expand V2-Shims Rollup Config (#898)
    * feat: add browser/node separation
    
    * feat: remove type = module
    
    * feat: ES6 V2-shims build
    
    * wip: transpiling to ditch ??
    
    * feat: stop bundling externals
    
    * feat: jest.config.cjs

--- packages/v2-shims/rollup.config.js
+++ packages/v2-shims/rollup.config.js
@@ -1,36 +1,91 @@
+import css from 'rollup-plugin-import-css'
+import json from '@rollup/plugin-json'
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
-import nodeResolve from '@rollup/plugin-node-resolve'
-import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import { dts } from 'rollup-plugin-dts'
+import terser from '@rollup/plugin-terser'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.cjs',
-      exports: 'auto',
-      sourcemap: false,
-      format: 'cjs',
-    },
-    {
-      exports: 'auto',
-      file: 'dist/index.mjs',
-      sourcemap: false,
-      format: 'es',
-    },
-    {
-      exports: 'auto',
-      sourcemap: false,
-      strict: true,
-      file: 'dist/index.js',
-      format: 'umd',
-      name: 'v2Shims',
-    },
-  ],
-  plugins: [
-    json(),
-    commonjs({ include: '../../node_modules/**' }),
-    nodeResolve({ browser: true }),
-    typescript({ compilerOptions: { outDir: 'dist' } }),
-  ],
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PROD = process.env.NODE_ENV === 'production'
+if (!PROD) {
+  console.log('Not in production mode - skipping minification')
 }
+
+const external = [
+  '@flatfile/api',
+  '@flatfile/blueprint',
+  '@flatfile/listener',
+  '@flatfile/plugin-record-hook',
+]
+
+function commonPlugins(browser) {
+  return [
+    json(),
+    css(),
+    commonjs({ requireReturnsDefault: 'auto' }),
+    resolve({ browser, preferBuiltins: !browser }),
+    typescript({
+      tsconfig: 'tsconfig.json',
+      outDir: 'dist',
+      declaration: false,
+      declarationMap: false,
+    }),
+    PROD ? terser() : null,
+  ]
+}
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'commonjs',
+        exports: 'auto',
+        file: 'dist/index.cjs',
+        sourcemap: false,
+      },
+      {
+        format: 'esm',
+        exports: 'auto',
+        file: 'dist/index.mjs',
+        sourcemap: true,
+      },
+      {
+        exports: 'auto',
+        sourcemap: false,
+        strict: true,
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'v2Shims',
+      },
+    ],
+    plugins: commonPlugins(false),
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        exports: 'auto',
+        file: 'dist/index.browser.cjs',
+        format: 'cjs',
+      },
+      {
+        exports: 'auto',
+        file: 'dist/index.browser.mjs',
+        sourcemap: false,
+        format: 'es',
+      },
+    ],
+    plugins: commonPlugins(true),
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+]
