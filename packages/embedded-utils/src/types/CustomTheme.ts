31c268cdec98835283b9ac839df53785bc6e623f
Jacqueline Mander <jacqueline@flatfile.io>
2024-03-27 09:55:11 -0400

    fix: update types (#73)
    * fix: upadte types
    
    * fix: optional

--- packages/embedded-utils/src/types/CustomTheme.ts
+++ packages/embedded-utils/src/types/CustomTheme.ts
@@ -1,23 +1,71 @@
 export type BaseCustomThemeProps = {
   root?: {
+    fontFamily?: string
     primaryColor?: string
     dangerColor?: string
     warningColor?: string
+    successColor?: string
+    /**
+      For a more cohesive aesthetic, use the same color for your Action and Primary color.
+      */
+    actionColor?: string
+    borderColor?: string
+    buttonBorderRadius?: string
+    badgeBorderColor?: string
+    badgeBorderRadius?: string
+    checkboxBorderColor?: string
+    checkboxBorderRadius?: string
+    /**
+     The border color for all dropdowns, text inputs, and context menus.
+     */
+    interactiveBorderColor?: string
+    /**
+     The border radius for all dropdowns, text inputs, and context menus.
+     */
+    interactiveBorderRadius?: string
+    tabstripActiveColor?: string
+    tabstripInactiveColor?: string
+    tabstripHoverTextColor?: string
+    tabstripHoverBorderColor?: string
+    modalBorderRadius?: string
+    pillBorderRadius?: string
+    popoverBackgroundColor?: string
+    popoverBorderRadius?: string
+    tooltipBackgroundColor?: string
   }
   sidebar?: {
     /**
      * We validate that this is a real image before rendering
      */
     logo?: string
-    textColor: string
-    titleColor: string
-    focusBgColor: string
-    focusTextColor: string
-    backgroundColor: string
-    footerTextColor: string
-    textUltralightColor: string
+    textColor?: string
+    titleColor?: string
+    focusBgColor?: string
+    focusTextColor?: string
+    backgroundColor?: string
+    footerTextColor?: string
+    /**
+     * @deprecated no longer in use
+     */
+    textUltralightColor?: string
+    borderColor?: string
+  }
+  document?: {
+    borderColor?: string
   }
   table: Partial<BaseCustomTableThemeProps>
+  /**
+   * Only available on pro plans and above
+   */
+  email?: {
+    logo?: string
+    textColor?: string
+    titleColor?: string
+    buttonBgColor?: string
+    buttonTextColor?: string
+    footerTextColor?: string
+    backgroundColor?: string
+  }
 }
 
 export type BaseCustomTableThemeProps = {
@@ -27,16 +75,37 @@ export type BaseCustomTableThemeProps = {
     }
     checkbox: {
       color?: string
+      borderColor?: string
     }
   }
 
   filters: {
     color: string
+    /**
+     * The border radius of the table filters
+     */
+    outerBorderRadius?: string
+    /**
+     * The border radius for the individual filters
+     */
+    innerBorderRadius?: string
+    /**
+     * defaults to none
+     */
+    outerBorder?: string
     active: {
       backgroundColor: string
     }
     error: {
       activeBackgroundColor: string
+    }
+  }
+
+  buttons?: {
+    iconColor?: string
+    pill?: {
+      color?: string
+      backgroundColor?: string
     }
   }
 
@@ -56,6 +125,7 @@ export type BaseCustomTableThemeProps = {
 
   indexColumn: {
     backgroundColor: string
+    color?: string
     selected: {
       color: string
       backgroundColor: string
@@ -69,6 +139,8 @@ export type BaseCustomTableThemeProps = {
     active: {
       borderColor: string
       spinnerColor: string
+      borderWidth?: string
+      borderShadow?: string
     }
   }
 
