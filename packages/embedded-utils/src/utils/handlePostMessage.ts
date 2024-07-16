fd2007552fc2c5ffb9be0dd56921215d8e3698e3
Nate Ferrero <nate@flatfile.io>
2024-07-16 00:18:09 -0400

    feat(javascript): add i18n to wrapper (#159)
    * feat(javascript): add i18n to wrapper
    
    * feat(javascript): load translations from postMessage

--- packages/embedded-utils/src/utils/handlePostMessage.ts
+++ packages/embedded-utils/src/utils/handlePostMessage.ts
@@ -8,18 +8,23 @@ export const handlePostMessage = (
   onInit?: (data: { localTranslations: Record<string, any> }) => void
 ) => {
   return (message: MessageEvent<{ flatfileEvent: FlatfileEvent }>) => {
+    if (message.data && 'localTranslations' in message.data) {
+      const localTranslations = message.data.localTranslations as Record<
+        string,
+        any
+      >
+      onInit?.({ localTranslations })
+      return
+    }
     const { flatfileEvent } = message.data
     if (!flatfileEvent) {
       return
     }
-    if (flatfileEvent.topic === 'space:opened') {
-      const { localTranslations } = flatfileEvent.payload
-      onInit?.({ localTranslations })
-    } else if (
+    if (
+      closeSpace &&
       flatfileEvent.topic === 'job:outcome-acknowledged' &&
       flatfileEvent.payload.status === 'complete' &&
-      flatfileEvent.payload.operation === closeSpace?.operation &&
-      closeSpace &&
+      flatfileEvent.payload.operation === closeSpace.operation &&
       typeof closeSpace.onClose === 'function'
     ) {
       closeSpace.onClose({ event: flatfileEvent })
