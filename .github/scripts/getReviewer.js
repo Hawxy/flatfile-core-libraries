629a162cba4ea3eef542d642b2e4e9a21568be75
Merit <11151764+meritmalling@users.noreply.github.com>
2024-08-20 10:33:41 -0700

    Update/changelog ci (#178)
    * Update changelog
    
    * Update changelog
    
    * WIP
    
    Signed-off-by: Merit <merit@flatfile.io>
    
    * Install updated version
    
    Signed-off-by: Merit <merit@flatfile.io>
    
    ---------
    
    Signed-off-by: Merit <merit@flatfile.io>

---
+++ .github/scripts/getReviewer.js
@@ -0,0 +1,50 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+import { Octokit } from "@octokit/rest"
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+const owner = process.env.OWNER
+const repo = process.env.REPO
+const tag = process.env.TAG
+
+function extractCommits(prDescription) {
+  const shaPattern = /-\s+([a-f0-9]{7,40}):/g;
+  const commitSHAs = [];
+  
+  let match;
+  while ((match = shaPattern.exec(prDescription)) !== null) {
+    commitSHAs.push(match[1].trim());
+  }
+  return commitSHAs;
+}
+
+async function getAuthor(ref) {
+  const { data } = await octokit.repos.getCommit({
+    owner,
+    repo,
+    ref,
+  })
+  return data.author.login;
+}
+
+async function getReviewers({ tag }) {
+  try {
+    const { data } = await octokit.repos.getReleaseByTag({
+      owner,
+      repo,
+      tag,
+    })
+
+    const commits = await extractCommits(data.body)
+    return await Promise.all(commits.map(getAuthor))
+
+  } catch (error) {
+    console.error(`Error getting reviewers for release ${tag}`, error)
+    return false
+  }
+}
+
+getReviewers({ tag })
