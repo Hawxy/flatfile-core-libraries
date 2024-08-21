a4bf502edd6fcb133776e60a676358a0fb8b9445
Merit <11151764+meritmalling@users.noreply.github.com>
2024-08-21 16:34:57 -0700

    Update script for use outside module (#179)
    Signed-off-by: Merit <merit@flatfile.io>

--- .github/scripts/getReviewer.js
+++ .github/scripts/getReviewer.js
@@ -1,50 +1,56 @@
-import dotenv from "dotenv"
-dotenv.config()
 
-import { Octokit } from "@octokit/rest"
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-})
+(async () => {
+  const dotenv = require("dotenv")
+  dotenv.config()
 
-const owner = process.env.OWNER
-const repo = process.env.REPO
-const tag = process.env.TAG
+  const { Octokit } = await import("@octokit/rest");
 
-function extractCommits(prDescription) {
-  const shaPattern = /-\s+([a-f0-9]{7,40}):/g;
-  const commitSHAs = [];
-  
-  let match;
-  while ((match = shaPattern.exec(prDescription)) !== null) {
-    commitSHAs.push(match[1].trim());
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  const owner = process.env.OWNER
+  const repo = process.env.REPO
+  const tag = process.env.TAG
+
+  function extractCommits(prDescription) {
+    const shaPattern = /-\s+([a-f0-9]{7,40}):/g;
+    const commitSHAs = [];
+    
+    let match;
+    while ((match = shaPattern.exec(prDescription)) !== null) {
+      commitSHAs.push(match[1].trim());
+    }
+    return commitSHAs;
   }
-  return commitSHAs;
-}
 
-async function getAuthor(ref) {
-  const { data } = await octokit.repos.getCommit({
-    owner,
-    repo,
-    ref,
-  })
-  return data.author.login;
-}
-
-async function getReviewers({ tag }) {
-  try {
-    const { data } = await octokit.repos.getReleaseByTag({
+  async function getAuthor(ref) {
+    const { data } = await octokit.repos.getCommit({
       owner,
       repo,
-      tag,
+      ref,
     })
-
-    const commits = await extractCommits(data.body)
-    return await Promise.all(commits.map(getAuthor))
-
-  } catch (error) {
-    console.error(`Error getting reviewers for release ${tag}`, error)
-    return false
+    return data.author.login;
   }
-}
 
-getReviewers({ tag })
+  async function getReviewers({ tag }) {
+    try {
+      const { data } = await octokit.repos.getReleaseByTag({
+        owner,
+        repo,
+        tag,
+      })
+
+      const commits = await extractCommits(data.body)
+      return await Promise.all(commits.map(getAuthor))
+
+    } catch (error) {
+      console.error(`Error getting reviewers for release ${tag}`, error)
+      return false
+    }
+  }
+
+  return getReviewers({ tag })
+
+})();
