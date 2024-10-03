517c95985cdde6acaec081f86edde5dc82d28f52
JT <Hawxy@users.noreply.github.com>
2024-10-03 21:29:35 +0800

    Fix missing vue typings

--- apps/vue/src/Simplified.vue
+++ apps/vue/src/Simplified.vue
@@ -1,9 +1,22 @@
+<template>
 
-<script lang="jsx">
-import { ref, onMounted, h, defineComponent } from 'vue';
+  <div>
+    <div class="description">
+      <button @click="toggleSpace()">{{ showSpace ? 'Close' : 'Open' }} space</button>
+    </div>
+
+    <div class="space-wrapper" v-if="showSpace">
+      <Space />
+    </div>
+  </div>
+
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
 import { initializeFlatfile } from '@flatfile/vue';
-import { workbook } from "./config";
-import { listener } from './listener'
+
 
 const SPACE_ID = 'us_sp_12314';
 const ENVIRONMENT_ID = 'us_env_1234';
@@ -29,76 +42,50 @@ const sheet = {
   ],
 }
 
-export default defineComponent({
-  setup() {
-    const showSpace = ref(false);
-    const publishableKey = 'sk_1234';
-    const environmentId = ENVIRONMENT_ID;
-    const spaceProps = ref({
-      name: 'Trste!',
-      environmentId,
-      publishableKey,
-      sheet,
-      onSubmit: async ({job, sheet}) => {
-        const data = await sheet.allData()
-        output.value = JSON.stringify(data, " ", 2)
-        console.log('onSubmit', data)
-      },
-      onRecordHook: (record, event) => {
-        const firstName = record.get('firstName')
-        const lastName = record.get('lastName')
-        if (firstName && !lastName) {
-          record.set('lastName', 'Rock')
-          record.addInfo('lastName', 'Welcome to the Rock fam')
-        }
-        return record
-      },
-      closeSpace: {
-        operation: 'submitActionFg',
-        onClose: () => { showSpace.value = false; },
-      },
-      themeConfig: { primaryColor: "#546a76", textColor: "#fff" },
-      userInfo: {
-        name: 'my space name'
-      },
-      spaceInfo: {
-        name: 'my space name'
-      },
-      displayAsModal: true,
-      spaceBody: {
-        metadata: {
-          random: 'param'
-        }
-      }
-    });
-
-    const { Space, OpenEmbed } = initializeFlatfile(spaceProps.value);
-
-    const toggleSpace = () => {
-      showSpace.value = !showSpace.value;
-      OpenEmbed()
-    };
-
-    return {
-      toggleSpace,
-      showSpace,
-      Space
+const showSpace = ref(false);
+const publishableKey = 'sk_1234';
+const environmentId = ENVIRONMENT_ID;
+const spaceProps = ref({
+  name: 'Trste!',
+  environmentId,
+  publishableKey,
+  sheet,
+  onSubmit: async ({ job, sheet }: { job: any, sheet: any }) => {
+    const data = await sheet.allData()
+    console.log('onSubmit', data)
+  },
+  onRecordHook: (record: any, event: any) => {
+    const firstName = record.get('firstName')
+    const lastName = record.get('lastName')
+    if (firstName && !lastName) {
+      record.set('lastName', 'Rock')
+      record.addInfo('lastName', 'Welcome to the Rock fam')
     }
+    return record
   },
-  render(props, ctx) {
-    const Space = props.Space
-
-    return (
-      <div>
-        <div class="description">
-          <button onClick={props.toggleSpace}>{ props.showSpace ? 'Close' : 'Open' } space</button>
-        </div>
-
-        {props.showSpace && <div class="space-wrapper">
-          <Space />
-        </div>}
-      </div>
-    )
+  closeSpace: {
+    operation: 'submitActionFg',
+    onClose: () => { showSpace.value = false; },
   },
+  themeConfig: { root: { primaryColor: "#546a76", textColor: "#fff" } },
+  userInfo: {
+    name: 'my space name'
+  },
+  spaceInfo: {
+    name: 'my space name'
+  },
+  displayAsModal: true,
+  spaceBody: {
+    metadata: {
+      random: 'param'
+    }
+  }
 });
+
+const { Space, OpenEmbed } = initializeFlatfile(spaceProps.value);
+
+const toggleSpace = () => {
+  showSpace.value = !showSpace.value;
+  OpenEmbed()
+};
 </script>
--- apps/vue/src/config.ts
+++ apps/vue/src/config.ts
@@ -1,4 +1,9 @@
-export const workbook = {
+import type {Flatfile} from "@flatfile/api"
+
+export const workbook:  Pick<
+Flatfile.CreateWorkbookConfig,
+'name' | 'labels' | 'sheets' | 'actions'
+> = {
   name: 'All Data',
   labels: ['pinned'],
   sheets: [
